5ab244d0e8efb5c2b20a1a9b9aabbde98a3c4cb0
Jay Hu <127647967+khic689@users.noreply.github.com>
2026-01-08 17:04:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // 2. 配置你的 GitHub 基本路徑
     // 格式：https://raw.githubusercontent.com/使用者名稱/倉庫名稱/分支名稱/
-    const baseUrl = 'https://raw.githubusercontent.com/khic689/11401_CS203A/main/';
+    const baseUrl = 'https://github.com/khic689/11401_CS203A';
 
     const contentDiv = document.getElementById('markdown-content');
     const buttons = document.querySelectorAll('.btn-note');
